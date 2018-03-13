e0681d73c20d322fc55b3b040f7e93e1b6bc7c2f
Alexa <alexadiaz@msn.com>
2018-03-12 20:33:24 -0400

    Version sin controllers

--- routes/web.php
+++ routes/web.php
@@ -11,14 +11,82 @@
 |
 */
 
+use Illuminate\Http\Request;
 
-Route::get('/', 'MenuController@show')->name('app.menu');
+// Route::get("/suma/{n1}/{n2}", function($n1, $n2){
+//     return view("mauro", ['res' => $n1 + $n2]);
+// })->name("pag.mauro");
 
-Route::get('/suma', 'OperacionesController@suma')->name('app.suma');
-Route::get('/resta', 'OperacionesController@resta')->name('app.resta');
-Route::get('/multiplicacion', 'OperacionesController@multiplicacion')->name('app.multiplicacion');
+// Route::get("/calc", function($edad){
+//     return view("calc");
+// });
 
-Route::get('/acumulado', 'ResultadoController@acumulado')->name('app.acumulado');
-Route::get('/resultado/{res}', 'ResultadoController@resultado')->name('app.resultado');
+Route::get('/', function () {
+    return view('menu');
+})-> name('app.menu');
 
-Route::post('/haceroperacion', 'OperacionesController@hacerOperacion')->name('app.haceroperacion');
+
+Route::get('/acumulado', function(){
+    $acc = session('acc');
+    $sub = session('sub');
+
+    return view('acumulado', [
+        'acumulado' => $acc,
+        'substraido' => $sub    
+    ]);
+})->name('app.acumulado');
+
+
+
+Route::get('/suma', function(){
+    return view('formulario', ['operacion' => 'suma']);
+})->name('app.suma');
+
+Route::get('/resta', function(){
+    return view('formulario', ['operacion' => 'resta']);
+})->name('app.resta');
+
+Route::get('/multiplicacion', function(){
+    return view('formulario', ['operacion' => 'multiplicacion']);
+})->name('app.multiplicacion');
+
+
+Route::get('/resultado/{res}', function($res){
+    $acc = session('acc');
+    $sub = session('sub');
+    return view('resultado', [
+        'res' => $res, 
+        'acumulado' => $acc,
+        'substraido' => $sub    
+    ]);
+})->name('app.resultado');
+
+
+
+Route::post('/haceroperacion', function(Request $request){
+
+    $n1 = $request->input('n1');
+    $n2 = $request->input('n2');
+
+    $operacion = $request->input("operacion");
+
+    if($operacion =="suma"){
+        $res = $n1 + $n2;
+    }
+    else if($operacion == "resta"){
+        $res = $n1 - $n2;
+    }
+    else if($operacion == "multiplicacion"){
+        $res = $n1 * $n2;
+    }
+
+    $acc = session('acc');
+    $sub = session('sub');
+
+    session([
+        'acc' => $acc + $res, 
+        'sub' => $sub - $res
+    ]);
+
+    return redirect()->route('app.resultado', ['res' => $res]);
+})->name('app.haceroperacion');
