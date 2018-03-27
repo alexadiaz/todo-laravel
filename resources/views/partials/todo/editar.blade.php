d956e4361f30ecb6f370d3ba83c287ae41ad0604
Alexa <alexadiaz@msn.com>
2018-03-26 23:17:07 -0400

    codigo eliminado

--- resources/views/partials/todo/editar.blade.php
+++
@@ -1,4 +0,0 @@
-<form action="{{route('app.editar')}}" method="POST" class='linea'>
-    {{ csrf_field() }}  
-    <button type='submit' name="id" value='{{$i}}'>editar</button>
-</form>
--- resources/views/partials/todo/eliminar.blade.php
+++
@@ -1,4 +0,0 @@
-<form action="{{route('app.eliminar')}}" method ="POST" class='linea'>
-    {{ csrf_field() }}
-    <button type='submit' name="id" value='{{$i}}'>eliminar</button>
-</form>
