ca1de40c98aed1e587537606a753431ddd13d929
Alexa <alexadiaz@msn.com>
2018-03-26 23:16:30 -0400

    adicionar link de cancelar a formulario de editar

--- resources/views/todo/form_editar.blade.php
+++ resources/views/todo/form_editar.blade.php
@@ -1,16 +1,12 @@
-@extends('layouts.formmaster')
-
-@section('titulo')
-    <h1>Todo Session</h1>
-@endsection
+@extends('layouts.form_master')
 
 @section('formulario')
     <h3>Modifique la tarea</h3>
-    
     <form action="{{route('app.modificar')}}" method='POST'>
         <input type="hidden" name='id' value="{{$id}}">
         <input type="text" name='tarea' value="{{$tarea}}">
         {{ csrf_field() }}
-        <button type="submit">Modificar</button>
+        <button type="submit">modificar</button>
+    <a href="{{route('app.form')}}">cancelar</a>
     </form>
 @endsection
