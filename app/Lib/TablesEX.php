0243c414243a1a0db1f3c534b5f944f87595ec44
Alexa <alexadiaz@msn.com>
2018-04-23 18:30:02 -0400

    consultas version 2 - proteger nombre de tablas y campos

---
+++ app/Lib/TablesEX.php
@@ -0,0 +1,23 @@
+<?php
+
+// namespace App\Lib;
+
+// class TablesEX {
+
+//     public static $tables = array(
+//         "tareas" => "tasks",
+//         "personas" => "personas"
+//     );
+
+//     public static $tareasColumns = array(
+//         "nombre" => "nombre",
+//         "activo" => "activo",
+//         "dias" => "days"
+//     );
+
+//     public static $personasColumns = array(
+//         "nombre" => "nombre",
+//         "edad" => "edad"
+//     );
+
+// }
